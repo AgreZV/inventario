9971f645173682f025ebec5b6204e70a5dd3898a
AgreZV-David <david.duran.toledo@gmail.com>
2021-09-21 19:49:40 -0300

    git config --global user.email "david.duran.toledo@gmail.com"

--- src/main/java/cl/control/inventario/InventarioApplication.java
+++ src/main/java/cl/control/inventario/InventarioApplication.java
@@ -2,8 +2,10 @@ package cl.control.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class InventarioApplication {
 
 	public static void main(String[] args) {
